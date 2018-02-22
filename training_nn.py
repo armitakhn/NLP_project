79f32a468acbb4978328325042d1f218a0bb2933
Minh Huong Le Nguyen <minh-huong.le-nguyen@polytechnique.edu>
2018-02-22 22:22:17 +0100

    Cleaning code

--- training_nn.py
+++ training_nn.py
@@ -1,186 +1,165 @@
-import csv
-import json
+##################################
+# ====== importing stuffs ====== #
+##################################
+import time
+
+start = time.time()
+
 import numpy as np
 
-import matplotlib.pyplot as plt
+from sklearn import model_selection as ms
 
-# from sklearn.decomposition import PCA
-# from sklearn.manifold import TSNE
+import os
+import csv
 
-from gensim.models.word2vec import Word2Vec
-
-from keras.models import Model, load_model
+import tensorflow
+import keras
 from keras import backend as K
-from keras.layers import Input, Embedding, Dropout, Conv1D, GlobalMaxPooling1D, Dense
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv1D, MaxPooling1D
+from keras import activations
+
+end = time.time()
+
+print("tensorflow version %s (should be at least 0.12.1)" % tensorflow.__version__)
+print("keras version %s (should be at least 2.0.7)" % keras.__version__)
+print('loading libraries takes %.4fs' % (end-start))
 
 
-####################
-# parameter values #
-####################
+def write_submission(filename, pred):
+  '''
+  Write prediction result in a submission file
+
+  Parameters
+  ----------
+  filename: name of submission file
+  pred: prediction array
+    
+  '''
+  prediction = []
+
+  idx = 0
+  for p in pred:
+    choice = np.argmax(p) # should be either 0 or 1
+    prediction.append((idx, choice))
+    idx += 1
+
+  with open(path_submission + filename, 'w', newline='') as f:
+    csv_out = csv.writer(f)
+    csv_out.writerow(['id','category'])
+    for row in prediction:
+      csv_out.writerow(row)
+
+##############################
+# ====== loading data ====== #
+##############################
+start = time.time()
 
 path_data = 'data/'
 path_submission = 'submission/'
 
-#nb_features = len(features) # number of features
-stpwd_thd = 10 # number of most frequent words to consider as stopwords
-do_static = False # non static CNN
-nb_filters = 100 # number of filters
-filter_size = 3 # size of each filter --> 3x3 matrix
-drop_rate = 0.3 # dropout rate
-batch_size = 64
-nb_epoch =  3 # number of episodes
-my_optimizer = 'adam' # use 'adam' of sklearn as optimizer
-my_patience = 1 # for early stopping strategy
+# Number of classes: binary --> YES/NO if there is an edge
+num_classes = 2
+
+# load training features (training data)
+features = np.genfromtxt(path_data + 'training_features.csv', delimiter=',',skip_header=1)
+training = np.genfromtxt(path_data + 'training_set.txt', dtype=str) # to get the label only
+labels = training[:, 2]
+
+x_train, x_test, y_train, y_test = ms.train_test_split(features, labels, test_size=0.25)
+
+input_shape = (x_train.shape[1], 1) # nb of features
+
+# load training & testing labels
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# ====== loading features for prediction ====== #
+x_pred = np.genfromtxt(path_data + 'testing_features.csv', delimiter=',',skip_header=1)
+x_pred = x_pred.reshape(x_pred.shape[0], x_pred.shape[1], 1)
+x_pred = x_pred.astype('float32')
+
+print('x_train shape:', x_train.shape)
+print('y_train shape:', y_train.shape)
+print('x_test shape:', x_test.shape)
+print('y_test shape:', y_test.shape)
+print('x_pred shape:', x_pred.shape)
+
+end = time.time()
+print('loading data takes %.4fs' % (end-start))
 
 
-print('\n=== parameter values: ===')
-print('non-static:',do_static)
-print('number of filters applied to each region:',nb_filters)
-print('filter size:',filter_size)
-print('dropout rate:',drop_rate)
-print('batch size:',batch_size)
-print('number of epochs:',nb_epoch)
-print('optimizer:',my_optimizer)
-print('patience:',my_patience)
-print('=== end parameter values ===')
+######################################
+# ====== building the network ====== #
+######################################
+start = time.time()
+
+ 
+# ====== FF-NN ====== #
+model_nn = Sequential()
+
+model_nn.add(Flatten(input_shape=input_shape))
+
+model_nn.add(Dense(30, activation='relu'))
+model_nn.add(Dense(40, activation='relu'))
+model_nn.add(Dense(50, activation='relu'))
+model_nn.add(Dense(60, activation='relu'))  
+model_nn.add(Dense(50, activation='relu'))
+model_nn.add(Dense(40, activation='relu'))
+model_nn.add(Dense(30, activation='relu'))
+model_nn.add(Dense(20, activation='relu'))
+
+model_nn.add(Dense(num_classes, activation='softmax'))
+# ====== FF-NN ====== #
 
 
-######################
-# load training data #
-######################
+# ====== CNN ====== #
+# model_cnn = Sequential()
 
-training = np.genfromtxt(path_data + 'training_set.txt', dtype=str)
+# model_cnn.add(Conv1D(64, (3), activation='relu', input_shape=input_shape))
+# model_cnn.add(MaxPooling1D(pool_size=2))
 
-print('\nTraining data loaded:', training.shape)
+# model_cnn.add(Conv1D(32, (3), activation='relu'))
+# model_cnn.add(MaxPooling1D(pool_size=2))
 
+# model_cnn.add(Dropout(rate=0.25))
+# model_cnn.add(Dense(16, activation='relu'))
 
-###############
-# load labels #
-###############
+# model_cnn.add(Dropout(rate=0.35))
+# model_cnn.add(Flatten())
+# model_cnn.add(Dense(num_classes, activation='softmax'))
+# ====== CNN ====== #
 
-labels = np.array(training[:, 2]) # get the labels
+model_nn.compile(loss=keras.losses.binary_crossentropy,
+                  optimizer=keras.optimizers.Adam(),
+                  metrics=['accuracy'])
 
-print('\nLabels loaded:', labels.shape)
-
-
-##########################
-# load training features #
-##########################
-
-training_features = np.array(np.genfromtxt(path_data + 'training_features.csv', delimiter=',',skip_header=1))
-
-print('\nTraining features loaded:', training_features.shape)
+model_nn.summary()
 
 
-#########################
-# load testing features #
-#########################
+batch_size = 32
+epochs = 40
 
-testing_features = np.array(np.genfromtxt(path_data + 'testing_features.csv', delimiter=',',skip_header=1))
+early_stopping = keras.callbacks.EarlyStopping(monitor='val_acc', mode='max', patience=2)
 
-print('\nTesting features loaded:', testing_features.shape)
+# Run the train
+history = model_nn.fit(x_train, y_train,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        verbose=1,
+                        validation_data=(x_test,y_test),
+                        callbacks=[early_stopping]
+                        )
 
+# Predict
+pred = model_nn.predict(x_pred, verbose=1)
 
-
-################
-# defining CNN #
-################
-
-nb_features = training_features.shape[1]
-
-print('\nDefining CNN...')
-
-my_input = Input(shape=(nb_features,))
-
-
-embedding = Embedding(
-  input_dim = nb_features+1,
-  output_dim = 20,
-  trainable = do_static,
-  ) (my_input)
-
-embedding_dropped = Dropout(drop_rate)(embedding) # apply dropout
-
-conv = Conv1D(
-  filters = nb_filters,
-  kernel_size = filter_size,
-  activation = 'relu',
-  )(embedding_dropped)
-
-pooled_conv = GlobalMaxPooling1D()(conv)
-
-pooled_conv_dropped = Dropout(drop_rate)(pooled_conv)
-
-# we finally project onto a single unit output layer with relu activation
-prob = Dense(1, activation='relu')(pooled_conv_dropped)
-
-model = Model(my_input, prob)
-
-# We add a vanilla hidden layer:
-# model.add(Dense(hidden_dims))
-# model.add(Dropout(0.2))
-# model.add(Activation('relu'))
-
-model.compile(optimizer = my_optimizer, loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
-
-print('\nModel compiled')
-
-# # you can access the layers of the model with model.layers
-# # then the input/output shape of each layer with, e.g., model.layers[0].input_shape or model.layers[0].output_shape
-
-# # nice as well:
-model.summary()
-
-print('\nTotal number of model parameters:',model.count_params())
-
-
-################
-# training CNN #
-################
-
-# ~160s/epoch on 4-core i7 CPU (on GPU much faster). Gets to 0.87 after 2 epochs
-# warning: on CPU, by default, will use all the cores of the machine
-
-early_stopping = EarlyStopping(monitor = 'val_acc', patience = my_patience, mode = 'max')
-
-# use the .fit() method with appropriate values for the arguments 'x', 'y', 'batch_size', 'epochs', 'validation_data', and 'callbacks'. 'validation_data' needs to be passed as a tuple, 'callbacks' as a list
-model.fit(
-  x = training_features, 
-  y = labels, 
-  batch_size = batch_size,
-  epochs = nb_epoch, 
-  shuffle = True,
-  callbacks = [early_stopping]
-)
-
-# # to reload model:
-# # model = load_model(path_to_save + name_save) # I do not have hdf5 module
-
-
-#######################################
-# identifying predictive text regions #
-#######################################
-
-# the feature maps that we find at the output of the convolutional layer, before pooling is applied
-# provide region embeddings (in an nb_filters-dimensional space)
-# there are (max_size-filter_size+1) regions of size filter_size in an input of size max_size
-
-predictions = model.predict(testing_features).tolist()
-
-filename = 'submission_cnn_01.csv'
-
-# write submission to file
-with open(path_submission + filename, 'wb') as f:
-  csv_out = csv.writer(f)
-  csv_out.writerow(['id','category'])
-  for row in predictions:
-    csv_out.writerow(row)
-
-
-# predictions are probabilities of belonging to class 1
-
-# get_softmax = K.function([model.layers[0].input,K.learning_phase()], [model.layers[6].output])
-# predictions = get_softmax([np.array(x_test[:100]),0])[0]
-
-# # note: you can also use directly: predictions = model.predict(x_test[:100]).tolist()
+write_submission('submission_ffnn_01.csv', pred)
